8f4ba7b1bc604398539e55ce0714235a2e50e930
Jerry Chanut <jerry.chanut@soprasteria.com>
2015-12-17 17:16:33 +0100

    wip

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 var app = module.exports = require("koa")();
 
+var html = 
+    `<html>
+        <head>
+            <meta charset="UTF-8">
+            <title>A small test page</title>
+            <script src="//localhost:9091"></script>
+        </head>
+        <body>
+            Page loaded at: <span id="demo"></span>
+        </body>
+        <script language="javascript">
+                document.getElementById('demo').innerHTML = new Date();
+        </script>
+    </html>`;
+
 app.use(function *(){
+    if(this.request.path === "/client"){
+		this.body = html;
+		return;
+	}
 	this.body = "Koa says Hi!";
 });
 
